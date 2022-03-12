adb89f18e827eb0e6a72afc190edf86e662be283
dariancmagee <dariancmagee@gmail.com>
2022-03-12 12:18:48 -0500

    Added new login and register pages under views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
+const jwt = require('jsonwebtoken');
 var router = express.Router();
+require('dotenv').config();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* LOGIN page */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* REGISTER page */
+router.get('/register', function(req, res, next) {
+  res.render('register');
 });
 
 module.exports = router;
