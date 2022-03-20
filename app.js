de9a859fc71b0ac7438b1fc4540ae7144fffa927
Kelli <kellistone217@gmail.com>
2022-03-19 20:10:55 -0600

    Misc fixes to resolve api connection issues

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var restaurantRouter = require('./routes/restaurant');
+var restaurantRouter = require('./routes/restaurants');
 var app = express();
 
 // view engine setup
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use('/restaurant', restaurantRouter);
+app.use('/restaurants', restaurantRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/restaurants.js
@@ -0,0 +1,45 @@
+var express = require('express');
+const jwt = require('jsonwebtoken');
+var router = express.Router();
+require('dotenv').config();
+const axios = require('axios');
+const axiosHeader = process.env.AXIOS_HEADER;
+
+
+/* SEARCH page */
+router.get('/', function(req, res, next) {
+  console.log("in here")
+  res.render('restaurant', {list: []})
+});
+
+
+// Results page 
+router.post('/', function(req, res, next) {
+  console.log("Body:", req.body)
+  
+  
+  // This gets the zipcode and type of food from the body object
+  const {zipcode, type_of_food} = req.body;
+  
+  var config = {
+    method: 'get',
+    url: `https://api.yelp.com/v3/businesses/search?location=${zipcode}&categories=${type_of_food}`,
+    headers: { 
+      'Authorization': `${axiosHeader}`
+
+    }
+  };
+  
+  axios(config)
+  .then(function (response) {
+    console.log(response.data.businesses)
+    res.render('restaurant', { listOfRestaurants: response.data.businesses })
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  
+});
+
+
+module.exports = router; 
